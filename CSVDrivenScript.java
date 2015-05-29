e1d97a48f23c25febda3a3b14a4c2536467e6eee
Shubham Arora <shubham.arora@comprotechnologies.com>
2015-05-29 17:01:32 +0530

    Init
    CSV Driven Scripting for collecting Test Data input from CSV Sheet

---
+++ CSVDrivenScript.java
@@ -0,0 +1,156 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+@RunWith(value = Parameterized.class)
+public class CSVDrivenScript {
+
+    private RemoteWebDriver driver;
+    DesiredCapabilities dc;
+    File file;
+    FileWriter fw;
+    BufferedWriter bw;
+    private String url, browser;
+
+    long start, finish, loadtime1;
+    Double loadtime;
+
+    @Before
+    public void setUp() throws Exception {
+	
+	launchBrowser();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	file = new File("src/test/resources/output.csv"); // OutputCSV
+	if (!file.exists()) {
+	    file.createNewFile();
+	}
+    }
+
+    @Parameters
+    public static Collection<String[]> testData() throws IOException {
+	return getTestData("src/test/resources/input.csv"); 
+    }
+		
+    public CSVDrivenScript(String browser, String url) {
+	this.browser = browser;
+	this.url = url;
+    }
+
+    public static Collection<String[]> getTestData(String fileName) throws IOException {
+	List<String[]> records = new ArrayList<String[]>();
+	String record;
+	BufferedReader file = new BufferedReader(new FileReader(fileName));
+		while ((record = file.readLine()) != null) {
+		String fields[] = record.split(",");
+		records.add(fields);
+	}
+	
+	file.close();
+	return records;
+    }
+
+    @Test
+    public void TC_SearchGoogle() throws Exception {
+
+	fw = new FileWriter(file.getAbsoluteFile(), true);
+	bw = new BufferedWriter(fw);
+	
+	driver.get(url);
+
+	try{
+	    synchronized (driver) {
+		driver.wait(2000);
+	    }
+	}
+	catch(NullPointerException npe) {npe.printStackTrace();}
+	catch(InterruptedException ie){ie.printStackTrace();}	
+	
+	// This will find the element search input box by name.
+	WebElement element = driver.findElement(By.name("q"));
+	
+	// Enter something to search for
+	element.sendKeys("Shubham Arora Github");
+
+	// Submit the form
+	element.submit();
+	
+	System.out.println("Launched - " + driver.getTitle());
+	
+	// populating in CSV sheet
+	bw.write(driver.getTitle());
+	bw.newLine();
+	bw.write("****************");
+	bw.newLine();
+	bw.flush();
+	bw.close();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+	driver.quit();
+    }
+        
+    // Launch browser depending upon the value defined in input CSV
+    public void launchBrowser() {
+	if(browser.toLowerCase().equals("chrome")) {
+		instantiateChrome();
+	}
+	else if(browser.toLowerCase().equals("mozilla")) {
+		instantiateMozilla();
+	}
+	else {
+		instantiateIE();
+	}
+    }
+    
+    // Instantiate Chrome Browser
+    public void instantiateChrome() 
+    {
+	System.setProperty("webdriver.chrome.driver", "src/lib/chromedriver.exe");
+	dc = DesiredCapabilities.chrome();
+
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments("test-type");
+	
+	dc.setCapability(ChromeOptions.CAPABILITY, options);
+	driver = new ChromeDriver();
+    }
+    
+    // Instantiate Mozilla Browser
+    public void instantiateMozilla() 
+    {
+	dc= DesiredCapabilities.firefox();  
+	driver = new FirefoxDriver();
+    }
+    
+    // Instantiate IE Browser - using driver version 2.45
+    public void instantiateIE() 
+    {
+	System.setProperty("webdriver.ie.driver","src/lib/IEDriverServer_2.45.exe");
+	dc = DesiredCapabilities.internetExplorer();
+	driver = new InternetExplorerDriver();
+    }
+}
